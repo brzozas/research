92503d9db283e27437c0a60212bc4dd0759e4237
Sebastian Brzozowski <sebastian.brzozowski@totaljobsgroup.com>
2016-04-14 11:10:18 +0200

    spring boot

--- src/main/java/brzozowski/sebastian/App.java
+++ src/main/java/brzozowski/sebastian/App.java
@@ -1,13 +1,21 @@
 package brzozowski.sebastian;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(App.class, args);
     }
 }
